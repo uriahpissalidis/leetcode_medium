013a32d04903374da2d52eb13ae0fd7a9eb803dc
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-06-13 22:32:00 -0400

    Add files via upload

---
+++ 120_Triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        for idx_row in range(1, len(triangle)):
+            triangle[idx_row][0] += triangle[idx_row-1][0]
+            triangle[idx_row][-1] += triangle[idx_row-1][-1]
+            for i in range(1, len(triangle[idx_row-1])):
+                triangle[idx_row][i] += min(triangle[idx_row-1][i-1], triangle[idx_row-1][i])
+        
+        return min(triangle[-1])
