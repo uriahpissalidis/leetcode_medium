1cbc03949301bfe55014baf3a7d4ff86a3ec8781
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-07-02 11:11:32 -0400

    Update 797_All_Paths_From_Source_to_Target.py

--- 797_All_Paths_From_Source_to_Target.py
+++ 797_All_Paths_From_Source_to_Target.py
@@ -1,5 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        """
+        [] 0
+        [0] 1
+        [0, 1] 3
+        result: [[0, 1, 3]]
+        [0] 2
+        [0, 2] 3
+        result: [[0, 1, 3], [0, 2, 3]]
+        """
+
         size = len(graph)
         result = []
         
