f99c1c2c8e3cd05d589dc3a89a142fb9626d9ed3
Uriah Pissalidis <44508430+uriahpissalidis@users.noreply.github.com>
2022-08-25 20:52:08 -0400

    Create 869_Reordered_Power_of_2.py

---
+++ 869_Reordered_Power_of_2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        l=sorted(list(str(n)))
+        for i in range(30):
+            a=2**i
+            b=sorted(list(str(a)))
+            if l==b:
+                return True
+        else:
+            return False
